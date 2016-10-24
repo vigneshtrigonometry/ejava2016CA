8e01d452a34d5cb4bc93fe54e0f86b9e0097531f
Vignesh Selvaraju <e0013453@u.nus.edu>
2016-10-24 15:36:19 +0800

    Entity classes and persistence unit added

---
+++ week03_ca/src/java/appointment/entity/Appointment.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package appointment.entity;
+
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="appointment")
+public class Appointment{
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String appt_id;
+    
+    @Lob
+    private String description;
+    
+    private Date appt_date;
+    
+    @ManyToOne
+    private People people;
+
+    public String getAppt_id() {
+        return appt_id;
+    }
+
+    public void setAppt_id(String appt_id) {
+        this.appt_id = appt_id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getAppt_date() {
+        return appt_date;
+    }
+
+    public void setAppt_date(Date appt_date) {
+        this.appt_date = appt_date;
+    }
+
+    public People getPeople() {
+        return people;
+    }
+
+    public void setPeople(People people) {
+        this.people = people;
+    }
+    
+}
---
+++ week03_ca/src/java/appointment/entity/People.java
@@ -0,0 +1,55 @@
+package appointment.entity;
+
+import java.util.Collection;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="people")
+public class People {
+    @Id
+    private String pid;
+    
+    private String name;
+    
+    private String email;
+    
+    @OneToMany
+    private Collection<Appointment> appointments;
+
+    public String getPid() {
+        return pid;
+    }
+
+    public void setPid(String pid) {
+        this.pid = pid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Collection<Appointment> getAppointments() {
+        return appointments;
+    }
+
+    public void setAppointments(Collection<Appointment> appointments) {
+        this.appointments = appointments;
+    }
+    
+    
+}
